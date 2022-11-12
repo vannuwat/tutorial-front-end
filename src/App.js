6a6b850ca6aa4e8dc88e6e091c7bc7723842815a
Vannuwat Suthantip <6222782409@g.siit.tu.ac.th>
2022-11-12 14:47:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios, * as others from 'axios';
 
 function App() {
+  
+  const [data, setData] = useState("Hello");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+      const {data: response} = await axios.get('http://localhost:5000/api/get/data');
+      setData(response);
+      } 
+      catch (error) {
+        console.error(error.message);
+      }
+    }
+    fetchData();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {data}
         </p>
         <a
           className="App-link"
